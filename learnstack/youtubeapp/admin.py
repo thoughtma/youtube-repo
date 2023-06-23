e740a5e03593409f999e5fb83a4389af10ad0d0b
Mridul Shrivastava <117090877+thoughtma@users.noreply.github.com>
2023-06-23 16:11:22 +0530

    Delete learnstack/youtubeapp directory

--- learnstack/youtubeapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from youtubeapp.models import SearchQuery, YouTubeVideo
-
-# Register your models here.
-admin.site.register(SearchQuery)
-admin.site.register(YouTubeVideo)
