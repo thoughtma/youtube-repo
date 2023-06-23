1543fc8da4888d8528d16413316299c21cda78d9
mridul <mridul@thoughtwin.com>
2023-06-23 17:00:34 +0530

    fix:put logic on one fucntion

--- learnstack/sites/models.py
+++ learnstack/sites/models.py
@@ -13,8 +13,8 @@ class YouTubeVideo(models.Model):
 
 class UdemyCourse(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.CharField(max_length=10)
+    thumbnail = models.URLField()
     link = models.URLField()
 
     def __str__(self):
